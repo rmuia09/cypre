5dccff455b5dce4f1a25fea8b43b940a45b4267c
rmuia09 <nrichardmuia@gmail.com>
2020-05-01 22:28:47 +0300

    originals

---
+++ cypress/integration/examples/simple.spec.js
@@ -0,0 +1,42 @@
+
+import { navigateTo } from "../../pages/navigationlinks"
+//import { logOutNotOnboardedUser } from '../pages/NavigationBar'
+
+describe('TBFX home page', () => {
+    before(function() {
+      cy.viewport(1600, 1050)
+      cy.visit('/');
+    })
+    
+    it('check all links!', () => {
+      navigateTo.HomePage()
+
+      //cy.get('#login-title').should('be.visible')
+      //cy.contains('#login-registerLnk').click()
+    //})
+
+    //it('check signup page!', () => {
+      navigateTo.SignupPage()
+      //cy.get('#login-registerLnk').click() 
+      //cy.get('#register-title').should('be.visible')
+      //cy.url().should ('include', '/register')
+    //})
+
+    //it('check signin page!', () => {
+      navigateTo.SigninPage()
+      //cy.get('#quickLinks-loginInHereLnk').click()  
+      //cy.url().should('include', '/signin')
+      //cy.contains('og in here').click()  
+    //})
+    //it('check reset password page!', () => {
+      navigateTo.ResetpwdPage()
+      //cy.get('#login-resetPasswordLnk').click()  
+      //cy.get('#resetPassword-pageTitle').should('be.visible') 
+      //cy.url().should ('include', '/reset-password')
+      //cy.url().should('include', '/signin')
+      //cy.contains('log in here').click()  
+    })
+    })
+
+
+
---
+++ cypress/pages/navigationlinks.js
@@ -0,0 +1,26 @@
+
+
+export class NavigationLink{
+
+    HomePage(){
+        cy.get('#login-title').should('be.visible')
+    }
+
+    SignupPage(){
+        cy.get('#login-registerLnk').click() 
+        cy.get('#register-title').should('be.visible')
+        cy.url().should ('include', '/register')
+    }
+
+    SigninPage(){
+        cy.get('#quickLinks-loginInHereLnk').click()  
+        cy.url().should('include', '/signin')
+    }
+
+    ResetpwdPage(){
+        cy.get('#login-resetPasswordLnk').click()  
+        cy.get('#resetPassword-pageTitle').should('be.visible') 
+        cy.url().should ('include', '/reset-password')
+}
+}
+export const navigateTo = new NavigationLink() 
